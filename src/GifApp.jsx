cd0dabb9d9b8dd60b421b4522bbf038f3e797f83
Gonzalo Gestri <gonzalogestri92299@gmail.com>
2023-11-20 17:28:41 -0300

    implementar un nuevo componente, gif grid

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { AddCategory } from './components/AddCategory'
+import { GifGrid } from "./components/GifGrid";
 
 export const GifApp = () => {
 
@@ -24,11 +25,13 @@ export const GifApp = () => {
 
       {/*Listado de Gif*/}
 
-      <ol>
-          {categories.map(category => {
-            return <li key={category}>{category}</li>
-          })}
-      </ol>
+      {
+        categories.map(category => 
+          <GifGrid
+            key={category}
+            category={category} />
+        )
+      }
       {/*Gif Item*/}
     </>
   )
