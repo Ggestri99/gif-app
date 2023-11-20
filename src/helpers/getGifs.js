32654af845dcb47a069465df119ab97fea501eaa
Gonzalo Gestri <gonzalogestri92299@gmail.com>
2023-11-20 18:01:46 -0300

    correccion con useEffect

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=PAuqDxbhhVNnzp7ObPsAi8A8UC08JOH4&q=${category}&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=PAuqDxbhhVNnzp7ObPsAi8A8UC08JOH4&q=${category}&limit=4`
     const resp = await fetch(url)
     const {data} = await resp.json()
     
