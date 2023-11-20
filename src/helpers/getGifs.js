024d769441266d9b044e1bbb10bcf18fecfd62ef
Gonzalo Gestri <gonzalogestri92299@gmail.com>
2023-11-20 17:50:32 -0300

    refactorizacion e implementar consumo de servicio

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=PAuqDxbhhVNnzp7ObPsAi8A8UC08JOH4&q=${category}&limit=20`
+    const resp = await fetch(url)
+    const {data} = await resp.json()
+    
+    const gifs = data.map((img)=>({
+        id: img.id,
+        title: img.title,
+        url:img.images.downsized_medium.url
+    }))
+
+    console.log(gifs)
+
+}
